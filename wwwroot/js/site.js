dcbb04f7a74d05cf262adebca259af240b47d5c5
AlvinKimata <alvinkimata@gmail.com>
2024-02-16 11:56:46 +0300

    create js functions for rendering parial views in Doctor's view in Admin

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,7 +3,7 @@
 
 // Write your JavaScript code.
 
-function togglePartialView() {
+function togglePopup() {
     var popup = document.getElementById("myPopup");
     if (popup.style.display === "none") {
         console.log("none");
@@ -13,3 +13,34 @@ function togglePartialView() {
         console.log("block");
     }
 }
+
+function togglePopup1() {
+    var popup1 = document.getElementById("myPopup1");
+    
+    if (popup1.style.display === "none") {
+        popup1.style.display = "block";
+    } else {
+        popup1.style.display = "none";
+    }
+}
+
+function togglePopup2() {
+    var popup2 = document.getElementById("myPopup2");
+    
+    if (popup2.style.display === "none") {
+        popup2.style.display = "block";
+    } else {
+        popup2.style.display = "none";
+    }
+}
+
+function togglePopup3() {
+    var popup3 = document.getElementById("myPopup3");
+    
+    if (popup3.style.display === "none") {
+        popup3.style.display = "block";
+    } else {
+        popup3.style.display = "none";
+    }
+}
+
