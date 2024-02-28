106116dff451e42cf007a923a532f75c362385f2
AlvinKimata <alvinkimata@gmail.com>
2024-02-28 12:32:07 +0300

    Add appointment job

--- Services/AppointmentJob.cs
+++ Services/AppointmentJob.cs
@@ -1,8 +1,28 @@
 using Microsoft.AspNetCore.SignalR;
 using Quartz;
+// using HemoTrack.Models;
 
 namespace HemoTrack.Services
 {
     public class AppointmentJob : IJob
+    {
+    
+        private static string _message = "Appointment message";
+        private readonly ILogger<ConconcurrentJob> _logger;
+        private readonly IHubContext<JobsHub> _hubContext;
+
+        public AppointmentJob(IHubContext<JobsHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var beginMessage = $"This is an {_message} at {DateTime.Now}";
+            await _hubContext.Clients.All.SendAsync("ConcurrentJobs", beginMessage);
+            _logger.LogInformation(beginMessage);
+        }
+    
+    }
     
 }
